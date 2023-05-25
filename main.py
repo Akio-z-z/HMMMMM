054e384d99cad69edda58bd9fd3e9523decd7bff
Akio <isac.lodberg@gmail.com>
2023-05-25 13:51:47 +0200

    hmmmm

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
 import msvcrt
+import time
 
 length = int(input("Enter the length of the hmmmmmm: "))
 sys.setrecursionlimit(int(input("Enter max lines of hmmmmmm: ")))
@@ -7,6 +8,7 @@ sys.setrecursionlimit(int(input("Enter max lines of hmmmmmm: ")))
 def up():
     try:
         for i in range(length):
+            time.sleep(0.05)
             print("h" + ("m"*i))
 
             if i == length-1:
@@ -21,6 +23,7 @@ def up():
 def down():
     try:
         for i in range(length):
+            time.sleep(0.05)
             print("h" + ("m"*(length-i)))
 
             if i == length-1:
