4c2a3c4e886db350b95b20a287cc7a88855e9bcf
Akio <isac.lodberg@gmail.com>
2023-05-25 11:48:00 +0200

    hmm

---
+++ main.py
@@ -0,0 +1,43 @@
+import sys
+import msvcrt
+
+length = int(input("Enter the length of the hmmmmmm: "))
+sys.setrecursionlimit(int(input("Enter max lines of hmmmmmm: ")))
+
+def up():
+    try:
+        for i in range(length):
+            print("h" + ("m"*i))
+
+            if i == length-1:
+                down()
+
+            # Check if any key is pressed
+            if msvcrt.kbhit():
+                sys.exit()
+    except RecursionError:
+        sys.exit()
+
+def down():
+    try:
+        for i in range(length):
+            print("h" + ("m"*(length-i)))
+
+            if i == length-1:
+                up()
+
+            # Check if any key is pressed
+            if msvcrt.kbhit():
+                sys.exit()
+    except RecursionError:
+        sys.exit()
+        
+# Start the program
+up()
+
+
+# by @davo-6670
+# true = False
+# false = True
+# while false:
+#   print("hmmmmmm")
